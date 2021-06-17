a56909c53b90b7905083ef909ab7d379bda749d0
Ben <byoo0522@gmail.com>
2021-06-17 13:34:45 -0700

    set up validations

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,6 +4,8 @@ const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
+// backend/utils/validation.js
+const { validationResult } = require('express-validator');
 
 
 // Sends a JWT Cookie
@@ -64,6 +66,27 @@ const requireAuth = [
     return next(err);
   },
 ];
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors
+      .array()
+      .map((error) => `${error.msg}`);
+
+    const err = Error('Bad request.');
+    err.errors = errors;
+    err.status = 400;
+    err.title = 'Bad request.';
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors,
+};
 
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
