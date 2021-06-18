a94845af423553ef90b9a86de83a54108e55c1fc
Ben <byoo0522@gmail.com>
2021-06-18 12:44:23 -0700

    add signup and login react components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
@@ -26,11 +26,11 @@ function Root() {
       </BrowserRouter>
     </Provider>
   );
-}
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -12,18 +12,44 @@ const setUser = (user) => {
   };
 };
 
+// remove user
 const removeUser = () => {
   return {
     type: REMOVE_USER,
   };
 };
 
+// restore user session
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+// login user
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
     method: 'POST',
     body: JSON.stringify({
       credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+// signup user
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
       password,
     }),
   });
