39341b442b3cf76948810940898ed2135e2fbd22
Ben <byoo0522@gmail.com>
2021-06-22 10:43:48 -0700

    add background image and complete some formatting

---
+++ frontend/src/components/HeadBanner/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import music-bars from '../..'
+import '../HeadBanner/HeadBanner.css';
+
+
+
+function HeadBanner() {
+  return (
+    <div className="head-banner">HeadBanner</div>
+  )
+}
+
+
+export default HeadBanner;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,6 +5,8 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
+// import '../../images/jam-jar-icon.jpg'
+
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
@@ -25,6 +27,7 @@ function Navigation({ isLoaded }) {
 
   return (
     <div className="nav">
+      {/* <img src="frontend/public/images/jam-jar-icon.jpg" al="JamCloud Icon"></img> */}
       <ul className="nav-container">
         <li className='home-link'>
           <NavLink exact to="/">Home</NavLink>
