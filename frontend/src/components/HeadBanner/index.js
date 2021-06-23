096f263f4638763ef43b17072a8f414b06dbdd64
Ben <byoo0522@gmail.com>
2021-06-22 17:11:42 -0700

    add banner header and text

--- frontend/src/components/HeadBanner/index.js
+++ frontend/src/components/HeadBanner/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import musicBars from '../../images/musicBars.jpg';
 import '../HeadBanner/HeadBanner.css';
 
 
@@ -8,6 +7,7 @@ function HeadBanner() {
   return (
     <div className="head-banner">
       <h1 className="head-banner-app-title">JamCloud</h1>
+      <h3 className="banner-text">Spread the Jam.</h3>
     </div>
   )
 }
