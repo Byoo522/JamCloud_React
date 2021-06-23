5593ced745a01cb04b2af63892826843afdd1958
Ben <byoo0522@gmail.com>
2021-06-23 13:57:21 -0700

    add album cards and style the card containers

--- frontend/src/components/AlbumCard/index.js
+++ frontend/src/components/AlbumCard/index.js
@@ -3,7 +3,18 @@ import '../AlbumCard/AlbumCard.css';
 
 function AlbumCard() {
   return (
-    <div>AlbumCard</div>
+    <div className="album-card-container">
+      <div className="card">card-1</div>
+      <div className="card">card-2</div>
+      <div className="card">card-3</div>
+      <div className="card">card-4</div>
+      <div className="card">card-5</div>
+      <div className="card">card-6</div>
+      <div className="card">card-7</div>
+      <div className="card">card-8</div>
+      <div className="card">card-9</div>
+      <div className="card">card-10</div>
+    </div>
   )
 }
 
